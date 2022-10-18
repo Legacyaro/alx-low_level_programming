8085a7ba836e6c5316f277bfd625ee16c22eaf81
legacyaro <samwellbusy387@gmail.com>
2022-10-17 17:10:21 -0700

    mm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
 /**
  * main - entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
