69a766accea7a94d1d209c24ff5898c5f88cd8a4
legacyaro <samwellbusy387@gmail.com>
2022-10-19 17:02:18 -0700

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase letters
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
