04472a2f4b0428386b2c744b76cfe6c9012d5e7f
legacyaro <samwellbusy387@gmail.com>
2022-10-28 18:45:00 -0700

    uuuuuuuuuuuuuuuu

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - makes small letters turn capital letters
+ * cap_string - capitalizes
  * @str: string
  * Return: pointer to string
  */
 char *cap_string(char *str)
 {
-	i = 0;
+	int i = 0;
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'
+))
 			i++;
 		if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
 			str[i] -= 32;
 		i++;
 	}
-
 	return (str);
-
 }
