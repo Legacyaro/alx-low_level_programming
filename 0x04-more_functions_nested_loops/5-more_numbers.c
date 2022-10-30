6d60da437f1dfcc1c275efe2c98071809a3ec6ac
legacyaro <samwellbusy387@gmail.com>
2022-10-30 15:35:22 -0700

    more nos.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,12 @@ void more_numbers(void)
 		{
 			if (num > 9)
 			{
-				_putchar((num / 10) + '\0');
+				_putchar((num / 10) + '0');
 			}
-			_putchar((num % 10) + '\0');
+			_putchar((num % 10) + '0');
 		}
 
 		_putchar('\n');
 	}
 }
+
